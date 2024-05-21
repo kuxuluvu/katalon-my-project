edaa61923eec576c7291e15b44aa0f629360341c
kuxuluvu <vuduc762@gmail.com>
2024-05-21 11:00:04 +0700

    add API TCs

---
+++ Test Suites/Test suite 30.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test suite 30</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0b2350d8-be94-4ed9-ad2c-01019614a897</testSuiteGuid>
+</TestSuiteEntity>
